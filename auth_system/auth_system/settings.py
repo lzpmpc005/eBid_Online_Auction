f5b84a38f05925bef86e27c8eee319385d65e236
ThomasLix7 <lihongtaoix7@gmail.com>
2024-04-30 08:08:05 +0200

    instructions on setting .env.local

--- auth_system/auth_system/settings.py
+++ auth_system/auth_system/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from os import getenv, path
 import dotenv
 from datetime import timedelta
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ if path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DEBUG', 'False') == 'True'
