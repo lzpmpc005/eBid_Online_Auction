2956dfe4730e2727e798ef05d4f49490690613f5
ThomasLix7 <lihongtaoix7@gmail.com>
2024-05-13 12:43:52 +0200

    add notification function for winners of auctions

--- online_auction/scripts/initialize.ts
+++ online_auction/scripts/initialize.ts
@@ -25,23 +25,4 @@ async function createCategories() {
   }
 }
 
-const account = {
-  card_number: "1234123412341234",
-  card_holder: "nobody",
-  cvv: "123",
-  balance: 1000,
-  expire_year: 2030,
-  expire_month: 11,
-};
-
-async function createBankAccount() {
-  try {
-    await axios.post("http://localhost:8080/bankx/create-account", account);
-    console.log(`Created bank account ${account.card_number}`);
-  } catch (error) {
-    console.log("Error creating the bank account", error);
-  }
-}
-
 createCategories();
-createBankAccount();
